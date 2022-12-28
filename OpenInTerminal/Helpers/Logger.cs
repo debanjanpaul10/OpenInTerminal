08e8b56d4d755f268cb6aace7dabd5c60659a0b9
Debanjan Paul <v-debpaul@microsoft.com>
2022-12-28 17:09:37 +0530

    Added proper path and proper windows terminal command

--- OpenInTerminal/Helpers/Logger.cs
+++ OpenInTerminal/Helpers/Logger.cs
@@ -8,23 +8,45 @@ namespace OpenInTerminal.Helpers
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
-
     using Microsoft;
     using Microsoft.VisualStudio.Shell;
     using Microsoft.VisualStudio.Shell.Interop;
 
+    /// <summary>
+    /// The Logger Class
+    /// </summary>
     public static class Logger
     {
+        /// <summary>
+        /// The Visual Studio Output Window Pane
+        /// </summary>
         private static IVsOutputWindowPane pane;
+
+        /// <summary>
+        /// The IService Provider
+        /// </summary>
         private static IServiceProvider _provider;
+
+        /// <summary>
+        /// The Name string
+        /// </summary>
         private static string _name;
 
+        /// <summary>
+        /// Initialize the class
+        /// </summary>
+        /// <param name="provider">The Package provider</param>
+        /// <param name="name">The name string</param>
         public static void Initialize(Package provider, string name)
         {
             _provider = provider;
             _name = name;
         }
 
+        /// <summary>
+        /// The Log method
+        /// </summary>
+        /// <param name="message">The message to be logged</param>
         public static void Log(string message)
         {
             if (string.IsNullOrEmpty(message))
@@ -43,6 +65,10 @@ namespace OpenInTerminal.Helpers
             }
         }
 
+        /// <summary>
+        /// The exception logger
+        /// </summary>
+        /// <param name="ex">The exception to be logged</param>
         public static void Log(Exception ex)
         {
             if (ex != null)
@@ -51,6 +77,10 @@ namespace OpenInTerminal.Helpers
             }
         }
 
+        /// <summary>
+        /// The Ensure Pane in VS Terminal
+        /// </summary>
+        /// <returns>The boolean flag</returns>
         private static bool EnsurePane()
         {
             if (pane == null)
--- OpenInTerminal/OpenInTerminalPackage.cs
+++ OpenInTerminal/OpenInTerminalPackage.cs
@@ -1,21 +1,38 @@
-﻿global using Community.VisualStudio.Toolkit;
-global using Microsoft.VisualStudio.Shell;
-global using System;
-global using Task = System.Threading.Tasks.Task;
-using System.Runtime.InteropServices;
-using System.Threading;
+﻿// ***********************************************************************
+// <copyright file="OpenInTerminalPackage.cs" company="Debanjan Paul">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// ***********************************************************************
+
 
 namespace OpenInTerminal
 {
+    using Community.VisualStudio.Toolkit;
+    using Microsoft.VisualStudio.Shell;
+    using System;
+    using System.Threading.Tasks;
+    using OpenInTerminal.Helpers;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+    /// <summary>
+    /// The OpenInTerminal Package
+    /// </summary>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(PackageGuids.guidOpenInTerminalCmdSetString)]
-    public sealed class OpenInTerminalPackage : ToolkitPackage
+    [ProvideOptionPage(typeof(Options), "Web", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
+    [Guid(PackageGuids.guidPackageString)]
+    public sealed class OpenInTerminalPackage : AsyncPackage
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            await this.RegisterCommandsAsync();
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            var options = (Options)GetDialogPage(typeof(Options));
+
+            Logger.Initialize(this, Vsix.Name);
+            OpenTerminalCommand.Initialize(this, options);
         }
     }
 }
